9c8a499b3863fa2ce7c361bb0785b5bf316aa71b
Dharmik Shah <dharm1k987@gmail.com>
2020-03-08 21:23:40 -0400

    added node modules properly

--- voting-service/app.js
+++ voting-service/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 // const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
