3f1aab0937a874837841f3d40bde7745e3edbfc5
Dharmik Shah <dharm1k987@gmail.com>
2020-03-11 10:37:28 -0400

    Added proper docker configuration

--- voting-service/db/index.js
+++ voting-service/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(`mongoose://${process.env.MONGO_USERNANE}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+  .connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`, { 
+    useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, user: `${process.env.MONGO_USERNAME}`, pass: `${process.env.MONGO_PASSWORD}`
+  })
+  .then(() => { console.log("Connected to db")})
   .catch((e) => {
     console.error('Connection error', e.message);
   });
