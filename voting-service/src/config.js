4557737614fa106e500af52ea4ec259e56b27400
Alvin Tang <alvin.tang@mail.utoronto.ca>
2020-04-10 18:28:21 -0400

    :wrench: Added internal api key and auth service base url as config variables.

--- voting-service/src/config.js
+++ voting-service/src/config.js
@@ -8,6 +8,8 @@ const { JWT_SECRET_KEY } = process.env;
 const DB_CONNECTION_STRING = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
 const DB_USERNAME = process.env.MONGO_USERNAME;
 const DB_PASSWORD = process.env.MONGO_PASSWORD;
+const AUTH_SERVICE_BASE_URL = process.env.AUTH_SERVICE_BASE_URL;
+const INTERNAL_API_KEY = process.env.INTERNAL_API_KEY;
 
 export {
   IN_PRODUCTION,
@@ -16,4 +18,6 @@ export {
   DB_CONNECTION_STRING,
   DB_USERNAME,
   DB_PASSWORD,
+  AUTH_SERVICE_BASE_URL,
+  INTERNAL_API_KEY
 };
