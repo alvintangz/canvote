64cb4207cead1100fb834c92c875f3d5132e6974
Alvin Tang <alvin.tang@mail.utoronto.ca>
2020-04-10 19:16:47 -0400

    :wrench: Updated config variables w/ default values.

--- voting-service/src/config.js
+++ voting-service/src/config.js
@@ -2,13 +2,16 @@ import env from 'dotenv';
 
 env.config();
 
-const IN_PRODUCTION = parseInt(process.env.PRODUCTION, 10) === 1;
+const IN_PRODUCTION = parseInt(process.env.PRODUCTION, 10) === 1 || 1;
 const PORT = process.env.PORT || 3002;
 const { JWT_SECRET_KEY } = process.env;
-const DB_CONNECTION_STRING = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
-const DB_USERNAME = process.env.MONGO_USERNAME;
+const DB_HOST = process.env.MONGO_HOST || 'localhost';
+const DB_PORT = process.env.MONGO_PORT || '27017';
+const DB_DATABASE = process.env.MONGO_DATABASE || 'canvote';
+const DB_CONNECTION_STRING = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+const DB_USERNAME = process.env.MONGO_USERNAME || 'canvote';
 const DB_PASSWORD = process.env.MONGO_PASSWORD;
-const AUTH_SERVICE_BASE_URL = process.env.AUTH_SERVICE_BASE_URL;
+const AUTH_SERVICE_BASE_URL = process.env.AUTH_SERVICE_BASE_URL || 'http://localhost:3001/api/v1';
 const INTERNAL_API_KEY = process.env.INTERNAL_API_KEY;
 
 export {
@@ -16,6 +19,9 @@ export {
   PORT,
   JWT_SECRET_KEY,
   DB_CONNECTION_STRING,
+  DB_PORT,
+  DB_HOST,
+  DB_DATABASE,
   DB_USERNAME,
   DB_PASSWORD,
   AUTH_SERVICE_BASE_URL,
